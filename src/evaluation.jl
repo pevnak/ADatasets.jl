21746ac3ef19ec2ae997ae23e55d10c02d39bb9f
pevnak <pevnak@gmail.com>
2018-07-20 12:48:57 +0200

    added accuracy at a particular quantile estimated from training data

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -9,8 +9,8 @@ using FileIO
     detection accuracy at particular false positive rates
 
 """
-detacc_at_fp(n, p, α = [0.005,0.01,0.05]) = mean(p' .> quantile(n,1 - α),2)
-
+detacc_at_fp(n::Vector{T}, p::Vector{T}, α = [0.005,0.01,0.05]) where {T<:Real} = mean(p' .> quantile(n,1 - α),2)
+detacc_at_fp(x::Vector{T}, y::Vector{Int}, α = [0.005,0.01,0.05]) where {T<:Real} = detacc_at_fp(x[y .== 1], x[y .== 2],α)
 
 """
     runtest(fit, ps, predicts, prnames,  dataset, anomaly_type, polution, variation, idir, odir, repetition = 1, steps = 50000)
@@ -25,14 +25,24 @@ function runtest(fit, ps, predicts, prnames,  dataset, anomaly_type, polution, v
   data = RandomBatches((train[1],),100,steps)
 
   results = mapreduce(vcat,ps) do p
-    mf,info = fit(data,p...)
-    aucs = DataFrame(prediction = prnames, 
-    	test_aucs = evaluate(predicts,mf,test),
-    	train_aucs = evaluate(predicts,mf,train),
-    	train_005 = evaluate(predicts,mf,subsampleanomalous(train,0.005,repetition)),
-    	train_01 = evaluate(predicts,mf,subsampleanomalous(train,0.01,repetition)),
+    m,info = fit(data,p...)
+
+    #calculate areas under the curve on training and testing data for different prediction functions
+    aucs = mapreduce(vcat,zip(predicts, prnames)) do i 
+      f, prname = i 
+      df = DataFrame(
+        prname = prname,
+      	auc_test = auc(f, m, test),
+      	auc_train = auc(f, m, train),
+      	auc_train005 = auc(f, m, subsampleanomalous(train,0.005,repetition)),
+      	auc_train01 = auc(f, m, subsampleanomalous(train,0.01,repetition))
     	)
-    join(info, aucs, kind = :cross)
+      df = hcat(df,fprstats(f, m, train,test,0.005))
+      df = hcat(df,fprstats(f, m, train,test,0.01))
+      hcat(df,fprstats(f, m, train,test,0.05))
+    end
+    aucs = join(info, aucs, kind = :cross)
+
   end
 
   ofname = joinpath(odir,dataset,@sprintf("%s_%s_%g_%s.jld",name,anomaly_type,polution,variation))
@@ -41,8 +51,19 @@ function runtest(fit, ps, predicts, prnames,  dataset, anomaly_type, polution, v
   append2file(ofname,"auc",results)
 end
 
+"""
+  fprstats(f, m, train,test,α)
 
+  puts threshold on false positive rate on training data (assuming most of them are normal)
+  and calculates on this threshold false positive rate and detection accuracy
 
-evaluate(predict::Function, data) = EvalCurves.auc(EvalCurves.roccurve(predict(data[1]), data[2] - 1)...)
-evaluate(predict::Function, m, data) = EvalCurves.auc(EvalCurves.roccurve(predict(m, data[1]), data[2] - 1)...)
-evaluate(predict::AbstractArray, m, data) = map(p -> evaluate(p, m, data),predict)
+"""
+function fprstats(f, m, train,test,α)
+  τ = quantile(f(m,train[1]), 1 - α)
+  o = f(m, test[1])
+  names = map(s -> Symbol(replace(@sprintf("%s_%g",s,α),".","")),["fpr","dacc"]) 
+  DataFrame([mean( o[test[2][:] .== 1] .- τ .> 0) detacc_at_fp(o,test[2],[α])[1]], names)
+end
+
+auc(predict::Function, data) = EvalCurves.auc(EvalCurves.roccurve(predict(data[1]), data[2] - 1)...)
+auc(predict::Function, m, data) = EvalCurves.auc(EvalCurves.roccurve(predict(m, data[1]), data[2] - 1)...)
